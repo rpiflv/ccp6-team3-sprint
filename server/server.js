e87009cc01926ed732efe12536a0e9c59d3ca259
Flavio Ripa <67497636+rpiflv@users.noreply.github.com>
2022-10-27 12:28:24 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-// const { getAllLists } = require("./handler/function");
+const { getAllLists } = require("./handler/function");
 
 const PORT = process.env.PORT || 8080;
 
