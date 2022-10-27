4e4b62827c99fb0de02b6969993b28c20fb64171
Flavio Ripa <67497636+rpiflv@users.noreply.github.com>
2022-10-27 12:00:19 +0900

    Update knexfile.js

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,24 +1,24 @@
 require('dotenv').config({path: '../.env.local'})
 
 module.exports = {
-//     development: {
-//         client: 'postgresql',
-//         connection: {
-//             database: process.env.PSQL_DATABASE,
-//             user: process.env.PSQL_USER,
-//             password: process.env.PSQL_PASSWORD
-//         },
-//         pool: {
-//             min: 2,
-//             max: 10
-//         },
-//         migrations: {
-//             directory: './db/migrations'
-//         },
-//         seeds: {
-//             directory: './db/seeds'
-//         }
-//     },
+    development: {
+        client: 'postgresql',
+        connection: {
+            database: process.env.PSQL_DATABASE,
+            user: process.env.PSQL_USER,
+            password: process.env.PSQL_PASSWORD
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            directory: './db/migrations'
+        },
+        seeds: {
+            directory: './db/seeds'
+        }
+    },
     production: {
         client: "postgresql",
         connection: process.env.DATABASE_URL,
