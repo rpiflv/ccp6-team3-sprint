4b4e4c12ae7385436ac0e3cff6fdca7099f16749
Adam Burrough <adamjamesburrough@gmail.com>
2022-10-29 09:08:19 +0900

    patch: add user functionality

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, "..", "build")));
+// app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
 //ROUTE for FRONTEND
