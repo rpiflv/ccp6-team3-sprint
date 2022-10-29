eadb984e18656331cc9c6b85c2c179bcb6ae794c
Flavio Ripa <67497636+rpiflv@users.noreply.github.com>
2022-10-29 09:53:26 +0900

    Update SingleList.js

--- src/components/SingleList.js
+++ src/components/SingleList.js
@@ -75,13 +75,13 @@ export default function SingleList(props) {
 							// 	console.log(e.target.checked);
 							// }}
 							>X</input>
-							<input
-								type="button"
-								onClick={async (e) => {
-									await axios.delete(`/api/lists/${selectedList.id}/${item.itemName}/delete`, { listId: selectedList.id, itemName: item.itemName })
-								}}
-							></input>
-							<button>X</button>
+// 							<input
+// 								type="button"
+// 								onClick={async (e) => {
+// 									await axios.delete(`/api/lists/${selectedList.id}/${item.itemName}/delete`, { listId: selectedList.id, itemName: item.itemName })
+// 								}}
+// 							></input>
+// 							<button>X</button>
 						</td>
 					</tr>
 				))}
